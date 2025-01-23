09718667a0e9da214476e37f8bfe46c584a9221c
Kolton <kguthrie2@stu.jsu.edu>
2025-01-23 13:24:58 -0600

    Added logic

--- app/src/main/java/com/jsu/cs408/lab2a/MainActivity.java
+++ app/src/main/java/com/jsu/cs408/lab2a/MainActivity.java
@@ -1,6 +1,8 @@
 package com.jsu.cs408.lab2a;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.jsu.cs408.lab2a.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private String[] mysteries;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        mysteries = getResources().getStringArray(R.array.mystery_strings);
+    }
+
+    public void clickedHelpButton(View v) {
+        binding.output.setText(getRandomMystery());
+    }
+
+    private String getRandomMystery() {
+        int rand = (int) Math.floor(Math.random() * mysteries.length);
+
+        return mysteries[rand];
     }
 }
